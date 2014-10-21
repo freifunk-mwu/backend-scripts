b5454926cc839578e202e018c7de00075a42df54
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-10-21 18:30:01 +0200

    Make the keybootstrap script work and add a github entry in local ssh config

--- photon/all_keybootstrap.py
+++ photon/all_keybootstrap.py
@@ -1,26 +1,52 @@
 
 from photon import Photon
-from photon.util.locations import search_location
-from photon.util.files import write_file
+from photon.util.locations import search_location, change_location
+from photon.util.files import write_file, read_file
+
+# p = Photon(config='ffmwu_config.yaml', summary='ffmwu_summary.yaml', meta='keybootstrap_meta.json', verbose=False)
+p = Photon(config='ffmwu_config.yaml', summary=None                   , meta='keybootstrap_meta.json', verbose=False)
+s = p.settings.get
+
+def mk_ssh_prv():
+    return p.m(
+        'generating new private ssh keypair',
+        cmdd=dict(cmd='ssh-keygen -t rsa -b 4096 -N "" -f %s' %(s['crypt']['ssh']['prv']), cwd=s['crypt']['ssh']['folder'], verbose=True),
+    )
+
+def get_ssh_pub():
+    if not search_location(s['crypt']['ssh']['prv']): mk_ssh_prv()
+
+    pub = p.m(
+        'generating new public ssh key from private',
+        cmdd=dict(cmd='ssh-keygen -f %s -y' %(s['crypt']['ssh']['prv']), cwd=s['crypt']['ssh']['folder'])
+    )
+    if pub.get('returncode') == 0:
+
+        if read_file(s['crypt']['ssh']['pub']) != pub.get('out'):
+            write_file(s['crypt']['ssh']['pub'], pub.get('out'))
+            p.m('wrote public ssh key', more=pub, verbose=True)
+
+        return pub.get('out')
+
+def add_github_to_ssh_config():
+
+    pub = get_ssh_pub()
+    if pub:
+        conf = read_file(s['crypt']['ssh']['conf'])
+        if conf and not 'Host %s' %(s['common']['gh_ident']) in conf:
+            conf += '''\n
+# freifunk-mwu github access
+Host {gh_ident}
+\tUser git
+\tHostname github.com
+\tPreferredAuthentications publickey
+\tIdentityFile ~/.ssh/{prv_s}
+\n'''.format(gh_ident=s['common']['gh_ident'], prv_s=s['crypt']['ssh']['prv_s'])
+
+            change_location(s['crypt']['ssh']['conf'], s['crypt']['ssh']['conf_b'])
+            write_file(s['crypt']['ssh']['conf'], conf)
+        p.m('Make sure this key is in a github account wich has access to required repositories', more=pub, verbose=True)
 
 if __name__ == '__main__':
 
-    p = Photon(config='ffmwu_config.yaml', summary='ffmwu_summary.yaml', meta='keybootstrap_meta.json', verbose=False)
-    s = p.settings.get
-
-    from pprint import pprint
-    pprint(s)
-
-    if not search_location(s['common']['ssh']['prv']): p.m(
-        'generating new private ssh keypair',
-        cmdd=dict(cmd='ssh-keygen -t rsa -b 4096 -N "" -f %s' %(s['common']['ssh']['prv']), cwd=s['common']['ssh']['folder']),
-        verbose=True
-    )
-    if not search_location(s['common']['ssh']['pub']):
-        pub = p.m(
-            'generating new public ssh key from private',
-            cmdd=dict(cmd='ssh-keygen -f %s -y' %(s['common']['ssh']['prv']), cwd=s['common']['ssh']['folder']),
-            verbose=True
-        )
-        if pub.get('returncode') == 0:
-            write_file(s['common']['ssh']['pub'], pub.get('out'))
+    add_github_to_ssh_config()
