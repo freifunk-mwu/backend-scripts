0daf10726927e476d05ebc36ae13dd24836b18d8
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-10-21 19:13:04 +0200

    Rename the keybootstrap script because it now sets up global git user and email

--- photon/all_gitbootstrap.py
+++ photon/all_gitbootstrap.py
@@ -24,7 +24,7 @@ def get_ssh_pub():
 
         if read_file(s['crypt']['ssh']['pub']) != pub.get('out'):
             write_file(s['crypt']['ssh']['pub'], pub.get('out'))
-            p.m('wrote public ssh key', more=pub, verbose=True)
+            p.m('wrote public ssh key', more=pub)
 
         return pub.get('out')
 
@@ -33,20 +33,28 @@ def add_github_to_ssh_config():
     pub = get_ssh_pub()
     if pub:
         conf = read_file(s['crypt']['ssh']['conf'])
-        if conf and not 'Host %s' %(s['common']['gh_ident']) in conf:
+        ks = 'Make sure this key is in a github account which has access to required repositories'
+        if not 'Host %s' %(s['common']['gh_ident']) in conf:
             conf += '''\n
 # freifunk-mwu github access
+## {ks}
 Host {gh_ident}
 \tUser git
 \tHostname github.com
 \tPreferredAuthentications publickey
 \tIdentityFile ~/.ssh/{prv_s}
-\n'''.format(gh_ident=s['common']['gh_ident'], prv_s=s['crypt']['ssh']['prv_s'])
+\n'''.format(ks=ks, gh_ident=s['common']['gh_ident'], prv_s=s['crypt']['ssh']['prv_s'])
 
             change_location(s['crypt']['ssh']['conf'], s['crypt']['ssh']['conf_b'])
             write_file(s['crypt']['ssh']['conf'], conf)
-        p.m('Make sure this key is in a github account wich has access to required repositories', more=pub, verbose=True)
+        else: p.m('skipping config modification - entry already present')
+        p.m('%s\n\n%s\n' %(ks, pub), verbose=True)
+
+def set_git_config():
+    p.m('setting git user.name', cmdd=dict(cmd='git config --global --replace-all user.name "%s"' %(s['common']['hostname'])))
+    p.m('setting git user.email', cmdd=dict(cmd='git config --global --replace-all user.email "%s@%s"' %(s['common']['hostname'], s['common']['domain'])))
 
 if __name__ == '__main__':
 
     add_github_to_ssh_config()
+    set_git_config()
