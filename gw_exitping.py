d2bca03963af48caf68cd3d9d78c6ea79e2f391e
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-10-25 17:51:03 +0200

    Due to popular request: also start/stop the dhcp server on exitVPN up/down

--- gw_exitping.py
+++ gw_exitping.py
@@ -13,17 +13,32 @@ if __name__ == '__main__':
     for community in s['common']['communities']:
         cif = s['batman'][community]['interface']
         cbw = s['batman'][community]['bandwidth']
+
         if uping.status['ratio'] <= s['exitping']['min_ratio'] or aping.status['ratio'] <= s['exitping']['min_ratio']:
+
+            p.m('%s - it seems you are not properly connected!' %(community), verbose=True)
             p.m(
-                '%s - it seems you are not properly connected!\nremoving batman server flag for %s' %(community, cif),
+                'removing batman server flag for %s' %(cif),
                 cmdd=dict(cmd='sudo batctl -m %s gw off' %(cif)),
                 more=dict(ping_urls=uping.status, ping_addresses=aping.status),
                 verbose=True
             )
-        else:
             p.m(
-                '%s - you are connected!\nsetting batman server flag for %s (bw: %s)' %(community, cif, cbw),
+                'stopping isc-dhcp-server',
+                cmdd=dict(cmd='sudo initctl stop isc-dhcp-server'),
+                verbose=True
+            )
+        else:
+
+            p.m('%s - you are connected!!' %(community), verbose=True)
+            p.m(
+                'setting batman server flag for %s (bw: %s)' %(community, cif, cbw),
                 cmdd=dict(cmd='sudo batctl -m %s gw server %s' %(cif, cbw)),
                 more=dict(ping_urls=uping.status, ping_addresses=aping.status),
+                verbose=True
+            )
+            p.m(
+                'starting isc-dhcp-server',
+                cmdd=dict(cmd='sudo initctl start isc-dhcp-server'),
                 verbose=True
             )
