863e65e087d77dfc9d021f8f4d5a51fe578aaf58
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-11-01 16:07:43 +0100

    Add some additional files to the snapshot and the crontab

--- snapshot_configs_all.py
+++ snapshot_configs_all.py
@@ -3,17 +3,24 @@
 def snapshot_configs():
     from os import path
     from photon.util.locations import change_location
+    from photon.util.files import write_file
     from common import pinit
 
     p, s = pinit('snapshot_configs', verbose=True)
 
     git = p.git_handler(s['configs']['local'], remote_url=s['configs']['remote'])
+    git.cleanup
 
     if p.settings.load('queue', s['configs']['queue']):
 
         change_location(s['configs']['target'], False, move=True)
-        for loc in s['queue'].get('locations'):
+
+        for loc in s['queue'].get('locations') + s['configs']['qadd']:
             change_location(loc, path.join(s['configs']['target'], loc.lstrip('/')))
+
+        crontab = p.m('getting crontab', cmdd=dict(cmd='crontab -l'), critical=False)
+        if crontab.get('returncode') == 0:
+            write_file(path.join(s['configs']['target'], 'crontab_-l'), crontab.get('out'))
 
         git.publish
 
