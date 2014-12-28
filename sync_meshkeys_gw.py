4d44bed9b8b8a3ee7343c3d8b9e4cd751e455332
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-12-28 14:11:44 +0100

    clean up ALL TEH SCRIPTS..

--- sync_meshkeys_gw.py
+++ sync_meshkeys_gw.py
@@ -3,13 +3,13 @@
 def sync_meshkeys():
     from common import pinit
 
-    p, s = pinit('sync_meshkeys', verbose=True)
+    photon, settings = pinit('sync_meshkeys', verbose=True)
 
-    for community in s['common']['communities']:
-        git = p.git_handler(s['fastd'][community]['local'], remote_url=s['fastd'][community]['remote'])
+    for community in settings['common']['communities']:
+        git = photon.git_handler(settings['fastd'][community]['local'], remote_url=settings['fastd'][community]['remote'])
         git.cleanup
 
-        fastd = p.signal_handler(s['fastd'][community]['pidfile'])
+        fastd = photon.signal_handler(settings['fastd'][community]['pidfile'])
         fastd.hup
 
         git.publish
