a80910044f754b53ee8a7aa69c5081a7e72db961
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-12-29 13:49:38 +0100

    cleanup done! (processing slowly, I know.. But hey, I've got holidays :)

--- update_icdns_conf_gw.py
+++ update_icdns_conf_gw.py
@@ -2,8 +2,6 @@
 
 def update_icdns_conf():
     '''
-        Jetzt mit Kommentaren!
-
         Ziel des Scripts ist es, eine Konfiguration für bind zu generieren, damit das Gateway am icvpn teilnehmen kann.
 
         Diese Script läuft periodisch, um den jeweils aktuellen Stand aus icvpn Repository in der Konfiguration abzubilden.
@@ -14,37 +12,37 @@ def update_icdns_conf():
     from common import pinit
 
     # Photon Instanz, und deren Settings aus common.pinit holen
-    p, s = pinit('update_icdns_conf', verbose=True)
+    photon, settings = pinit('update_icdns_conf', verbose=True)
 
-    # Photon (``p``) dazu veranlassen, jeweils einen ``git_handler`` mit den entsprechenden Werten aus seinen Settings (``s``) für
+    # Photon dazu veranlassen, jeweils einen ``git_handler`` mit den entsprechenden Werten aus seinen Settings für
     # die 'scripts' und für 'meta' zu starten. Auf diese Handler-Funktionen jeweils ``_pull()`` in dem Repo aufrufen (a.k.a `git pull`)
-    for r in ['scripts', 'meta']:
-        p.git_handler(s['icvpn']['icdns'][r]['local'], remote_url=s['icvpn']['icdns'][r]['remote'])._pull()
+    for repo in ['scripts', 'meta']:
+        photon.git_handler(settings['icvpn']['icdns'][repo]['local'], remote_url=settings['icvpn']['icdns'][repo]['remote'])._pull()
 
     # Photon legt unter ``bc`` einen ``template_handler`` ab.
     # Der Inhalt der Templates ist nur eine Variable: ``conf``
-    bc = p.template_handler('${conf}')
+    bc = photon.template_handler('${conf}')
 
-    # Mit ``p.m`` wird das ``mkdns`` Script aus dem icvpn-meta Repository aufgerufen
+    # Mit ``photon.m`` wird das ``mkdns`` Script aus dem icvpn-meta Repository aufgerufen
     # Im ``cmdd`` steht der eigentliche Aufruf (``cmd``) sowie der Pfad (``cwd``) im dem das Kommando ausgeführt wird
     # Schlägt das Kommando fehl, schaltet Photon den ganzen Python-Interpreter mit hübsch Fehlermeldung ab.
     # Ansonsten: Vor Augen halten, was hier passiert: Im Python 3 Interpreter innerhalb einer Shell läuft diese Script hier,
     # welches eine weitere Shell startet um darin ein Python2 Script auszuführen. Dazu noch ohne den Code vorher zu prüfen..
     # Der Ausgabe-String (``get('out')``) wird jedenfalls mit ``bc.sub`` unter dem Namen ``conf`` abgelegt.
-    bc.sub = dict(conf=p.m(
+    bc.sub = dict(conf=photon.m(
         'genarating dns conf',
-        cmdd=dict(cmd='./mkdns -f bind -s %s -x mainz -x wiesbaden' %(s['icvpn']['icdns']['meta']['local']), cwd=s['icvpn']['icdns']['scripts']['local'])
+        cmdd=dict(cmd='./mkdns -f bind -s %s -x mainz -x wiesbaden' %(settings['icvpn']['icdns']['meta']['local']), cwd=settings['icvpn']['icdns']['scripts']['local'])
     ).get('out'))
 
     # ``bc.sub`` setzt alle hinterlegten Werte (``conf``) in das Template ein und gibt dieses als String zurück.
     # Hat sich dieser gegenüber der momentan aktiven Konfiguration (``read_file``) geändert (``!=``)..
-    conf = s['icvpn']['icdns']['conf']
+    conf = settings['icvpn']['icdns']['conf']
     if bc.sub != read_file(conf):
         # .. wird die Konfiguration danach mit der aus dem Template-Handler (``bc.write``) ersetzt (``append=False``).
         bc.write(conf, append=False)
 
         # reload
-        p.m(
+        photon.m(
             'reloading dns daemon',
             cmdd=dict(cmd='sudo rndc reload')
         )
--- update_tinc_conf_gw.py
+++ update_tinc_conf_gw.py
@@ -2,8 +2,6 @@
 
 def update_tinc_conf():
     '''
-        Jetzt mit Kommentaren!
-
         Ziel des Scripts ist es, eine Konfiguration für tinc zu generieren, damit das Gateway am icvpn teilnehmen kann.
 
         Diese Script läuft periodisch, um den jeweils aktuellen Stand aus icvpn Repository in der Konfiguration abzubilden.
@@ -15,25 +13,25 @@ def update_tinc_conf():
     from common import pinit
 
     # Photon Instanz, und deren Settings aus common.pinit holen
-    p, s = pinit('update_tinc_conf', verbose=True)
+    photon, settings = pinit('update_tinc_conf', verbose=True)
 
-    # Photon (``p``) dazu veranlassen, einen ``git_handler`` mit Werten aus seinen Settings (``s``) zu starten
+    # Photon dazu veranlassen, einen ``git_handler`` mit Werten aus seinen Settings zu starten
     # Mit den Parametern ``local`` weiss der Handler bescheid, wo er das Repo ablegen/wiederfinden soll,
     # Die ``remote_url`` wird dazu genutzt, das Repository von dieser Adresse zu klonen, liegt bereits unter
     # ``local`` ein Repository wird für alle Aktionen die darin hinterlegte Adresse genutzt
     # (== die ``remote_url`` mit der initial geklont wurde)
     # Erst danach wird die Handler-Funktion ``_pull()`` in dem Repo aufgerufen (a.k.a `git pull`)
-    p.git_handler(s['icvpn']['tinc']['local'], remote_url=s['icvpn']['tinc']['remote'])._pull()
+    photon.git_handler(settings['icvpn']['tinc']['local'], remote_url=settings['icvpn']['tinc']['remote'])._pull()
 
     # Das Gateway muss wissen, wie es sich dem icvpn gegenüber zu melden hat.
-    # In den Settings von Photon (``s``) steht unter ``['icvpn']['tinc']['iamdb']``
+    # In den Settings von Photon (``settings``) steht unter ``['icvpn']['tinc']['iamdb']``
     # eine Zuordnung der Hostnamen zu den entsprechenden Namen.
     # Das Settings Modul kann auf Keywords in der Konfigurationsdatei reagieren, z.B. wird der Hostname
     # unter ``['common']['hostname']`` in den Settings abgelegt. Diesen nutzen wir um unseren icvpn-Namen
     # aus der Zuordnung zu erhalten: ``iam``. Wird nichts gefunden (``iam == None``) darf Photon den
     # Gehsteig hochklappen: Fehlermeldungen werfen und den ganzen Python-Interpreter abschalten (``state=True``)
-    iam = s['icvpn']['tinc']['iamdb'][s['common']['hostname']] if s['common']['hostname'] in s['icvpn']['tinc']['iamdb'] else None
-    if not iam: p.m('Host not present in configuration!', more=dict(host=s['common']['hostname']), state=True)
+    iam = settings['icvpn']['tinc']['iamdb'][settings['common']['hostname']] if settings['common']['hostname'] in settings['icvpn']['tinc']['iamdb'] else None
+    if not iam: photon.m('Host not present in configuration!', more=dict(host=settings['common']['hostname']), state=True)
 
     # Wohin soll sich tinc verbinden? Dies steht in den tinc-Keys der anderen icvpn Teilnehmer.
     # Ein Key kann aber auf mehrere Adressen verweisen, sollte aber nur einmal in der Konfiguration auftauchen.
@@ -41,11 +39,11 @@ def update_tinc_conf():
     connects = set()
 
     # Einmal durch alle tinc-Keys aus dem Repository gehen
-    for key in listdir(s['icvpn']['tinc']['hosts']):
+    for key in listdir(settings['icvpn']['tinc']['hosts']):
         # Den eigenen Key ignorieren
         if key.lower() != iam.lower():
             # Den Key mit der Photon-Hilfs-Funktion ``read_file`` einlesen, danach zeilenweise (``split``) durchgehen
-            for line in read_file(path.join(s['icvpn']['tinc']['hosts'], key)).split('\n'):
+            for line in read_file(path.join(settings['icvpn']['tinc']['hosts'], key)).split('\n'):
                 # Beginnt eine Zeile innerhalb des Keys mit ``address`` oder ``Address`` (oder z.B. mit ``AdDrEsS`` ;)) ..
                 if line.lower().startswith('address'):
                     # .. kommt der Dateiname des Keys in das set
@@ -56,27 +54,27 @@ def update_tinc_conf():
     # ``tinc.conf.tpl`` im Unterordner ``common`` ein und hinterlegt die oben ermittelten Werte (``fields``).
     # Die ``connects`` werden zum String gewandelt, indem alle
     # Elemente im Set mit einer Leerzeile verbunden werden (``'\n'.join``)..
-    tc = p.template_handler(
+    tc = photon.template_handler(
         path.join(path.dirname(__file__), 'common', 'tinc.conf.tpl'),
         fields=dict(
             iam=iam,
-            interface=s['icvpn']['interface'],
+            interface=settings['icvpn']['interface'],
             connects='\n'.join(sorted(connects))
         )
     )
 
     # ``tc.sub`` setzt alle unter ``fields`` hinterlegte Werte in das Template ein und gibt dieses als String zurück.
     # Hat sich dieser gegenüber der momentan aktiven Konfiguration (``read_file``) geändert (``!=``)..
-    if tc.sub != read_file(s['icvpn']['tinc']['conf']):
+    if tc.sub != read_file(settings['icvpn']['tinc']['conf']):
         # .. wird die Konfiguration danach mit der aus dem Template-Handler (``bc.write``) ersetzt (``append=False``).
-        tc.write(s['icvpn']['tinc']['conf'], append=False)
+        tc.write(settings['icvpn']['tinc']['conf'], append=False)
 
         # Offenbar ist leider nur ein restart die wirklich stabile Variante,
         # tinc zum Beachten der neuen config zu bringen _und_ überhaupt
         # sicherzustellen, dass tinc auch läuft.
-        p.m(
+        photon.m(
             'restarting tinc daemon',
-            cmdd=dict(cmd='sudo service %s restart' %(s['icvpn']['tinc']['exec']))
+            cmdd=dict(cmd='sudo service %s restart' %(settings['icvpn']['tinc']['exec']))
         )
 
 if __name__ == '__main__':
