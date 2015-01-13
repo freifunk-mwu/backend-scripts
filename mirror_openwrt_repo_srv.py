63ec31f29b13f4e55ac12ace5f1afce97e7b30d9
Kokel <tobias@hachmer.de>
2015-01-13 12:20:30 +0100

    use lftp with command file

--- mirror_openwrt_repo_srv.py
+++ mirror_openwrt_repo_srv.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 def mirror_openwrt_repo():
+    from os import path
     from photon.util.files import read_file
     from common import pinit
 
     photon, settings = pinit('mirror_openwrt_repo', verbose=True)
 
+    lftp_conf = path.join(path.dirname(__file__), 'common', settings['openwrt']['lftp_conf'])
+
     config_content=photon.m(
         'mirror openwrt repo dir %s%s' %(settings['openwrt']['remote_repo_url'], settings['openwrt']['bb_stable_dir']),
         cmdd=dict(
-            cmd='lftp %s -e "mirror --delete %s %s"' %(settings['openwrt']['remote_repo_url'], settings['openwrt']['bb_stable_dir'], settings['openwrt']['local_repo_dir']),
+            cmd='lftp -f %s' %(lftp_conf),
         )
     ).get('out')
 
