b9a697d08c6ba5ce74024ef3ce729464a4d9ccca
ka-ba <ka-ba@users.noreply.github.com>
2015-04-21 15:10:37 +0200

    added mkroa
    mkroa eingebaut;
    wenn ohne typo, dann sollte es tun, aber:
    !!! ungetestet bisher !!!

--- update_bird_conf_gw.py
+++ update_bird_conf_gw.py
@@ -13,22 +13,42 @@ def update_bird_conf():
         )._pull()
 
     for ip_ver in settings['icvpn']['bird']['ip_ver']:
-
+        do_restart = False
         bird_conf = photon.template_handler('${config_content}')
-        config_content=photon.m(
-            'generating ip_ver%s bgp conf' %(ip_ver),
+
+        # peers
+        peers_config_content=photon.m(
+            'generating ip_ver%s bgp peers conf' %(ip_ver),
             cmdd=dict(
                 cmd='./mkbgp -f bird -%s -s %s -x mainz -x wiesbaden -d ebgp_ic' %(ip_ver, settings['icvpn']['bird']['meta']['local']),
                 cwd=settings['icvpn']['bird']['scripts']['local']
             )
         ).get('out')
-        bird_conf.sub = dict(config_content=config_content)
+        bird_peers_conf.sub = dict(peers_config_content=peers_config_content)
 
-        conf = settings['icvpn']['bird']['ip_ver'][ip_ver]['conf']
+        peers_conf = settings['icvpn']['bird']['ip_ver'][ip_ver]['peers_conf']
 
-        if bird_conf.sub != read_file(conf):
-            bird_conf.write(conf, append=False)
+        if bird_peers_conf.sub != read_file(peers_conf):
+            bird_peers_conf.write(peers_conf, append=False)
+            do_restart = True
 
+        # roa
+        roa_config_content=photon.m(
+            'generating ip_ver%s bgp roa conf' %(ip_ver),
+            cmdd=dict(
+                cmd='./mkroa -f bird -%s -s %s -x mainz -x wiesbaden' %(ip_ver, settings['icvpn']['bird']['meta']['local']),
+                cwd=settings['icvpn']['bird']['scripts']['local']
+            )
+        ).get('out')
+        bird_roa_conf.sub = dict(roa_config_content=roa_config_content)
+
+        roa_conf = settings['icvpn']['bird']['ip_ver'][ip_ver]['roa_conf']
+
+        if bird_roa_conf.sub != read_file(roa_conf):
+            bird_roa_conf.write(roa_conf, append=False)
+            do_restart = True
+
+        if do_restart:
             photon.m(
                 'restarting bird daemon for v%s' %(ip_ver),
                 cmdd=dict(
