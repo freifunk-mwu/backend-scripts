f938bf95a2a01e7d130314f59a2d4683c57a4f3e
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-10-20 09:37:21 +0200

    Cleanup tinc script, remove unused imports, reformat syntax

--- update_tinc_conf_gw.py
+++ update_tinc_conf_gw.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-def update_tinc_conf():
-    from os import listdir, path
-    from photon.util.files import read_file
-    from common import pinit
+from common import pinit
 
+
+def update_tinc_conf():
     photon, settings = pinit('update_tinc_conf', verbose=True)
 
-    photon.git_handler(settings['icvpn']['tinc']['local'], remote_url=settings['icvpn']['tinc']['remote'])._pull()
+    photon.git_handler(
+        settings['icvpn']['tinc']['local'],
+        remote_url=settings['icvpn']['tinc']['remote']
+    )._pull()
 
 if __name__ == '__main__':
     update_tinc_conf()
