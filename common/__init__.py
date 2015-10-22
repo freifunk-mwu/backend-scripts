bcdbe6b5657dd3b4bdeb9435ae275b2a197ab83c
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-10-22 12:17:23 +0200

    Move imports out out pinit method, add some inline documentation

--- common/__init__.py
+++ common/__init__.py
@@ -1,14 +1,28 @@
+'''
+**common/ffmwu_defaults.yaml**
+
+.. literalinclude:: ../../common/ffmwu_defaults.yaml
+    :language: yaml
+    :linenos:
+
+'''
+from os import path
+
+from photon import Photon
+
 
 def pinit(mname, verbose=True):
+    '''
+    Prepares a Photon Instance to load the ``ffmwu_defaults.yaml`` file.
 
-    from os import path
-    from photon import Photon
-
+    On the first run of Photon the ``ffmwu_config.yaml`` file is generated, so
+    custom changes can be applied here.
+    '''
     cwd = path.dirname(__file__)
     photon = Photon(
         path.join(cwd, 'ffmwu_defaults.yaml'),
         config=path.join(cwd, 'ffmwu_config.yaml'),
-        meta='%s_meta.json' %(mname),
+        meta='%s_meta.json' % (mname),
         verbose=verbose
     )
     return photon, photon.settings.get
