0420428cff87b9bf360a2df6779bb98908f85b83
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-10-25 12:31:19 +0100

    Do not leave Photon instances in global void in bootstrap git script

--- bootstrap_git_all.py
+++ bootstrap_git_all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from common import pinit
+from photon.util.files import read_file, write_file
+from photon.util.locations import search_location
 
-photon, settings = pinit('bootstrap_git', verbose=True)
+from common import pinit
 
 DESC = 'Make sure this key is associated with the "freifunkmwu" user on github'
 SSH_TPL = '''\n
@@ -15,44 +16,46 @@ Host ${gh_ident}
 \tIdentityFile ~/.ssh/${prv_s}
 \n'''
 
-def mkprv_ssh():
-    return photon.m(
-        'generating new private ssh keypair',
-        cmdd=dict(
-            cmd='ssh-keygen -t rsa -b 4096 -N "" -f %s' %(settings['crypt']['ssh']['prv']),
-            cwd=settings['crypt']['ssh']['folder'], verbose=True
-        )
-    )
-
-def getpub_ssh():
-    from photon.util.locations import search_location
-    from photon.util.files import write_file, read_file
-
-    if not search_location(settings['crypt']['ssh']['prv']):
-        mkprv_ssh()
-
-    pub = photon.m(
-        'generating new public ssh key from private',
-        cmdd=dict(
-            cmd='ssh-keygen -f %s -y' %(settings['crypt']['ssh']['prv']),
-            cwd=settings['crypt']['ssh']['folder']
-        )
-    )
-    if not pub.get('returncode') == 0:
-        photon.m('Error creating public ssh key', state=False)
-
-    if read_file(settings['crypt']['ssh']['pub']) != pub.get('out'):
-        write_file(settings['crypt']['ssh']['pub'], pub.get('out'))
-        photon.m('wrote public ssh key', more=pub)
-
-    return pub.get('out')
 
 def bootstrap_git():
+    photon, settings = pinit('bootstrap_git', verbose=True)
+
+    def mkprv_ssh():
+        return photon.m(
+            'generating new private ssh keypair',
+            cmdd=dict(
+                cmd='ssh-keygen -t rsa -b 4096 -N "" -f %s' % (
+                    settings['crypt']['ssh']['prv']
+                ),
+                cwd=settings['crypt']['ssh']['folder'], verbose=True
+            )
+        )
+
+    def getpub_ssh():
+        if not search_location(settings['crypt']['ssh']['prv']):
+            mkprv_ssh()
+
+        pub = photon.m(
+            'generating new public ssh key from private',
+            cmdd=dict(
+                cmd='ssh-keygen -f %s -y' % (settings['crypt']['ssh']['prv']),
+                cwd=settings['crypt']['ssh']['folder']
+            )
+        )
+        if not pub.get('returncode') == 0:
+            photon.m('Error creating public ssh key', state=False)
+
+        if read_file(settings['crypt']['ssh']['pub']) != pub.get('out'):
+            write_file(settings['crypt']['ssh']['pub'], pub.get('out'))
+            photon.m('wrote public ssh key', more=pub)
+
+        return pub.get('out')
 
     pub = getpub_ssh()
     if not pub:
         photon.m('Can not bootstrap git configuration', state=True)
 
+    # add entry to config
     conf_t = photon.template_handler(SSH_TPL)
     conf_t.sub = dict(
         desc=DESC,
@@ -64,13 +67,18 @@ def bootstrap_git():
     photon.m(
         'setting git user.name',
         cmdd=dict(
-            cmd='git config --global --replace-all user.name "%s"' %(settings['common']['hostname'])
+            cmd='git config --global --replace-all user.name "%s"' % (
+                settings['common']['hostname']
+            )
         )
     )
     photon.m(
         'setting git user.email',
         cmdd=dict(
-            cmd='git config --global --replace-all user.email "%s@%s"' %(settings['common']['hostname'], settings['common']['domain'])
+            cmd='git config --global --replace-all user.email "%s@%s"' % (
+                settings['common']['hostname'],
+                settings['common']['domain']
+            )
         )
     )
 
@@ -81,10 +89,11 @@ def bootstrap_git():
             desc=DESC,
             pub=pub,
             local=settings['common']['mailto']['local'],
-            br='\n%s\n' %('~'*8)
+            br='\n%s\n' % ('~'*8)
         )
     )
     photon.m(shell_t.sub, verbose=True)
+
 
 if __name__ == '__main__':
     bootstrap_git()
