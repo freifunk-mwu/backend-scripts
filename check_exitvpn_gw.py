cd8336948ee86b08ff7282f569011f5741b70895
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-11-19 13:34:36 +0100

    Only attempt to start/stop isc-dhcp only if not/already running in exitvpn script

--- check_exitvpn_gw.py
+++ check_exitvpn_gw.py
@@ -12,8 +12,8 @@ def check_exitvpn():
 
     ping = photon.ping_handler(net_if=settings['exitping']['interface'])
 
+    photon.m('sending pings', more=settings['exitping']['targets'])
     ping.probe = settings['exitping']['targets']
-
     photon.m('ping results', more=ping.status)
 
     for community in settings['common']['communities']:
@@ -21,43 +21,42 @@ def check_exitvpn():
         bandwidth = settings['batman'][community]['bandwidth']
 
         if ping.status['ratio'] <= settings['exitping']['min_ratio']:
-            photon.m(
-                'exitvpn for %s - it seems you are not properly connected!' % (
-                    community
-                )
-            )
+            photon.m('exitvpn for %s - you are _not_ connected!' % (community))
+
             photon.m(
                 'removing batman server flag for %s' % (interface),
-                cmdd=dict(
-                    cmd='sudo batctl -m %s gw off' % (interface)
-                )
+                cmdd=dict(cmd='sudo batctl -m %s gw off' % (interface))
             )
-            photon.m(
-                'stopping isc-dhcp-server',
-                cmdd=dict(
-                    cmd='sudo initctl stop isc-dhcp-server'
+
+            if 'start/running' in photon.m(
+                'check if isc-dhcp-server is running to stop it',
+                cmdd=dict(cmd='initctl status isc-dhcp-server')
+            ).get('out', ''):
+                photon.m(
+                    'stopping isc-dhcp-server',
+                    cmdd=dict(cmd='sudo initctl stop isc-dhcp-server')
                 )
-            )
 
         else:
             photon.m('exitvpn for %s - you are connected!!' % (community))
+
             photon.m(
                 'setting batman server flag for %s (bw: %s)' % (
                     interface, bandwidth
                 ),
-                cmdd=dict(
-                    cmd='sudo batctl -m %s gw server %s' % (
-                        interface, bandwidth
-                    )
-                ),
+                cmdd=dict(cmd='sudo batctl -m %s gw server %s' % (
+                    interface, bandwidth
+                )),
             )
-            photon.m(
-                'starting isc-dhcp-server',
-                cmdd=dict(
-                    cmd='sudo initctl start isc-dhcp-server'
-                ),
-                critical=False
-            )
+
+            if 'stop/waiting' in photon.m(
+                'check if isc-dhcp-server is not running to start it',
+                cmdd=dict(cmd='initctl status isc-dhcp-server')
+            ).get('out', ''):
+                photon.m(
+                    'starting isc-dhcp-server',
+                    cmdd=dict(cmd='sudo initctl start isc-dhcp-server')
+                )
 
 
 if __name__ == '__main__':
