ebfcb8e6a0c2f22f18e7aae6492fedfae3492589
Tobias Hachmer <tobias@hachmer.de>
2019-03-25 20:59:25 +0100

    sync_meshkeys_gw.sh: add dom[0-7]

--- sync_meshkeys_gw.sh
+++ sync_meshkeys_gw.sh
@@ -4,7 +4,7 @@
 set -e
 
 # sites to sync
-SITES="mz mzig wi wiig"
+SITES="mz mzig wi wiig dom0 dom1 dom2 dom3 dom4 dom5 dom6 dom7"
 
 # site definitions
 mz_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
@@ -18,6 +18,31 @@ wi_LOCAL="/etc/fastd/wivpn-1406/peers"
 
 wiig_REMOTE="https://github.com/freifunk-mwu/ffwi-infrastructure-peers.git"
 wiig_LOCAL="/etc/fastd/wiigvpn-1406/peers"
+
+dom0_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom0_LOCAL="/etc/fastd/dom0vpn-1406/peers"
+
+dom1_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom1_LOCAL="/etc/fastd/dom1vpn-1406/peers"
+
+dom2_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom2_LOCAL="/etc/fastd/dom2vpn-1406/peers"
+
+dom3_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom3_LOCAL="/etc/fastd/dom3vpn-1406/peers"
+
+dom4_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom4_LOCAL="/etc/fastd/dom4vpn-1406/peers"
+
+dom5_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom5_LOCAL="/etc/fastd/dom5vpn-1406/peers"
+
+dom6_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom6_LOCAL="/etc/fastd/dom6vpn-1406/peers"
+
+dom7_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
+dom7_LOCAL="/etc/fastd/dom7vpn-1406/peers"
+
 
 # sync git repositories for all sites
 for SITE in ${SITES}; do
