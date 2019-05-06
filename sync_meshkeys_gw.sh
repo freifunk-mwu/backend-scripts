fcee6db3af81629fd1813ab95dc41300d196c8ed
Julian Labus <julian@labus-online.de>
2019-05-06 17:53:24 +0200

    sync_meshkeys_gw.sh: remove sync of obsolete folders

--- sync_meshkeys_gw.sh
+++ sync_meshkeys_gw.sh
@@ -4,48 +4,17 @@
 set -e
 
 # sites to sync
-SITES="mwu mz mzig wi wiig dom0 dom1 dom2 dom3 dom4 dom5 dom6 dom7"
+SITES="mwu mzig wiig"
 
 # site definitions
 mwu_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
 mwu_LOCAL="/home/admin/clones/peers-ffmwu"
 
-mz_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-mz_LOCAL="/etc/fastd/mzvpn-1406/peers"
-
 mzig_REMOTE="https://github.com/freifunk-mwu/ffmz-infrastructure-peers.git"
 mzig_LOCAL="/etc/fastd/mzigvpn-1406/peers"
 
-wi_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-wi_LOCAL="/etc/fastd/wivpn-1406/peers"
-
 wiig_REMOTE="https://github.com/freifunk-mwu/ffwi-infrastructure-peers.git"
 wiig_LOCAL="/etc/fastd/wiigvpn-1406/peers"
-
-dom0_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom0_LOCAL="/etc/fastd/dom0vpn-1406/peers"
-
-dom1_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom1_LOCAL="/etc/fastd/dom1vpn-1406/peers"
-
-dom2_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom2_LOCAL="/etc/fastd/dom2vpn-1406/peers"
-
-dom3_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom3_LOCAL="/etc/fastd/dom3vpn-1406/peers"
-
-dom4_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom4_LOCAL="/etc/fastd/dom4vpn-1406/peers"
-
-dom5_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom5_LOCAL="/etc/fastd/dom5vpn-1406/peers"
-
-dom6_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom6_LOCAL="/etc/fastd/dom6vpn-1406/peers"
-
-dom7_REMOTE="https://github.com/freifunk-mwu/peers-ffmwu.git"
-dom7_LOCAL="/etc/fastd/dom7vpn-1406/peers"
-
 
 # sync git repositories for all sites
 for SITE in ${SITES}; do
